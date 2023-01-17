d277619eab9a135f104876153b5c81958f584beb
Jev <32514140+devjevnl@users.noreply.github.com>
2023-01-17 13:01:17 +0100

    more renaming

--- powershell/functions/Add-AzAdNewApplicationOwnerInBulk.ps1
+++ powershell/functions/Add-AzAdNewApplicationOwnerInBulk.ps1
@@ -22,7 +22,7 @@
     Source      : https://github.com/thecloudexplorers/simply-scripted
 #>
 
-function Add-NewApplicationOwnerInBulk {
+function Add-AzAdNewApplicationOwnerInBulk {
     [CmdLetBinding()]
     param (
         [Parameter(Mandatory)]
