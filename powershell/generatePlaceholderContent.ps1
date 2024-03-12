3db55699e343c44dd682bd4657fb737c7011c71f
Jev <32514140+devjevnl@users.noreply.github.com>
2024-03-12 21:52:24 +0100

    added iteration for caledar events

--- powershell/generatePlaceholderContent.ps1
+++ powershell/generatePlaceholderContent.ps1
@@ -1,5 +1,6 @@
 
-$nrOfEmailsToGenerate = 10
+$nrOfEmailsToGenerate = 200
+$nrOfEventsToGenerate = 500
 $sourceUpn = ""
 $targetUpn = ""
 $targetName = ""
@@ -11,43 +12,50 @@ Import-Module Microsoft.Graph.Calendar
 . .\powershell\functions\New-PlaceholderText.ps1
 . .\powershell\functions\Get-StartAndEndDate.ps1
 
-$eventLocationName = New-PlaceholderText -Number 5 -Words
-$eventContent = New-PlaceholderText -Number 10 -Paragraphs
 
-$params = @{
-    subject               = $eventLocationName
-    body                  = @{
-        contentType = "Text"
-        content     = $eventContent
-    }
-    start                 = @{
-        dateTime = "2024-03-12T12:00:00"
-        timeZone = $timeZone
-    }
-    end                   = @{
-        dateTime = "2024-03-12T14:00:00"
-        timeZone = $timeZone
-    }
-    location              = @{
-        displayName = $eventLocationName
-    }
-    attendees             = @(
-        @{
-            emailAddress = @{
-                address = $targetUpn
-                name    = $targetName
-            }
-            type         = "required"
+for ($i = 0; $i -lt $nrOfEventsToGenerate; $i++) {
+    <# Action that will repeat until the condition is met #>
+
+    $eventLocationName = New-PlaceholderText -Number 5 -Words
+    $eventContent = New-PlaceholderText -Number 10 -Paragraphs
+
+    $eventDates = Get-StartAndEndDate -TimeSpanStartDate $(Get-Date) -TimeSpanEndDate $(Get-Date).AddYears(2)
+
+    $params = @{
+        subject               = $eventLocationName
+        body                  = @{
+            contentType = "Text"
+            content     = $eventContent
         }
-    )
-    isOnlineMeeting       = $true
-    onlineMeetingProvider = "teamsForBusiness"
+        start                 = @{
+            dateTime = $eventDates.RandomStartDate
+            timeZone = $timeZone
+        }
+        end                   = @{
+            dateTime = $eventDates.RandomEndDate
+            timeZone = $timeZone
+        }
+        location              = @{
+            displayName = $eventLocationName
+        }
+        attendees             = @(
+            @{
+                emailAddress = @{
+                    address = $targetUpn
+                    name    = $targetName
+                }
+                type         = "required"
+            }
+        )
+        isOnlineMeeting       = $true
+        onlineMeetingProvider = "teamsForBusiness"
+    }
+
+    # A UPN can also be used as -UserId.
+    $defaultCalendar = Get-MgUserCalendar -UserId $sourceUpn | Where-Object Name -eq 'Calendar'
+    New-MgUserCalendarEvent -UserId $sourceUpn -CalendarId $defaultCalendar.Id -BodyParameter $params > $null
+    Write-Host "Event [$i/$nrOfEventsToGenerate] created for $targetUpn starting at [$($eventDates.RandomStartDate)] ending [$($eventDates.RandomEndDate)] with title $eventLocationName"
 }
-
-# A UPN can also be used as -UserId.
-
-$defaultCalendar = Get-MgUserCalendar -UserId $sourceUpn | Where-Object Name -eq 'Calendar'
-New-MgUserCalendarEvent -UserId $sourceUpn -CalendarId $defaultCalendar.Id -BodyParameter $params
 
 for ($i = 0; $i -lt $nrOfEmailsToGenerate; $i++) {
     $mailTitle = New-PlaceholderText -Number 5 -Words
@@ -80,6 +88,6 @@ for ($i = 0; $i -lt $nrOfEmailsToGenerate; $i++) {
 
     # A UPN can also be used as -UserId.
     # https://learn.microsoft.com/en-us/graph/api/user-sendmail?view=graph-rest-1.0&tabs=powershell#example-1-send-a-new-email-using-json-format
-    Send-MgUserMail -UserId $sourceUpn -BodyParameter $params
-    Write-Host "Mail sent to $targetUpn from $sourceUpn with title $mailTitle"
+    Send-MgUserMail -UserId $sourceUpn -BodyParameter $params > $null
+    Write-Host "Mail [$i/$nrOfEmailsToGenerate] sent to $targetUpn from $sourceUpn with title $mailTitle"
 }
