f5135007415a51cbbc814ce9a791e4fcb31903c5
Jev <32514140+devjevnl@users.noreply.github.com>
2024-06-26 13:04:03 +0200

    initial check-in

---
+++ powershell/functions/Get-VsCodeExtensionMetadata.ps1
@@ -0,0 +1,84 @@
+<#
+    .SYNOPSIS
+    Retrieves metadata for a specified Visual Studio Code extension from the Visual Studio Marketplace.
+
+    .DESCRIPTION
+    The `Get-VsCodeExtensionMetadata` function queries the Visual Studio Marketplace API to retrieve metadata for a specified VS Code extension.
+    The metadata includes the publisher name, extension name, version, and asset URI.
+    This function requires an extension ID as input and returns a custom object with the retrieved metadata.
+
+    .PARAMETER ExtensionId
+    The unique identifier of the Visual Studio Code extension. This parameter is required.
+
+    .EXAMPLE
+    PS> $metadata = Get-VsCodeExtensionMetadata -ExtensionId "ms-python.python"
+    PS> $metadata
+    Publisher     : ms-python
+    ExtensionName : python
+    Version       : 2023.10.1
+    AssetUri      : https://example.com/assetUri
+
+    .OUTPUTS
+    System.Management.Automation.PSCustomObject
+    The function returns a custom object with the following properties:
+    - Publisher: The name of the publisher of the extension.
+    - ExtensionName: The name of the extension.
+    - Version: The version of the extension.
+    - AssetUri: The URI for the extension's assets.
+
+    .NOTES
+    The function uses the Visual Studio Marketplace API to retrieve the extension metadata.
+    This API is undocumented and may change without notice, use it at your own risk.
+#>
+
+function Get-VsCodeExtensionMetadata {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [System.String] $ExtensionId
+    )
+
+    try {
+        $apiUrl = "https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version=7.2-preview"
+        $requestBody = @{
+            filters    = @(
+                @{
+                    criteria = @(
+                        @{
+                            filterType = 7
+                            value      = $ExtensionId
+                        }
+                    )
+                }
+            )
+            # IncludeLatestVersionOnly (512), IncludeAssetUri (128), IncludeStatistics (16) = 656
+            # Details on flags https://learn.microsoft.com/en-us/javascript/api/azure-devops-extension-api/extensionqueryflags?view=azdevops-ext-latest
+            flags      = 656
+            assetTypes = @("Microsoft.VisualStudio.Services.VSIXPackage")
+        } | ConvertTo-Json -Depth 5
+
+        # Invoke the REST API to retrieve the extension metadata.
+        $response = Invoke-RestMethod -Uri $apiUrl -Method POST -ContentType "application/json" -Body $requestBody -StatusCodeVariable 'statusCode'
+
+        if ($statusCode -ne 200) {
+            throw "Failed to retrieve extension metadata. Http StatusCode: [$statusCode]"
+        }
+
+        if (-not $response.results[0].extensions) {
+            throw "No extensions found with the ID [$ExtensionId]."
+        }
+
+        # Extract the metadata from the response.
+        $metadata = [PSCustomObject]@{
+            Publisher   = $response.results[0].extensions[0].publisher.publisherName
+            Name        = $response.results[0].extensions[0].extensionName
+            Version     = $response.results[0].extensions[0].versions[0].version
+            DownloadUri = "$($response.results[0].extensions[0].versions[0].assetUri)/Microsoft.VisualStudio.Services.VSIXPackage"
+        }
+
+        return $metadata
+
+    } catch {
+        Write-Error "Unable to complete request: [$($_.Exception.Message)]"
+    }
+}
---
+++ powershell/installVsCodeExtensionsFromFile.ps1
@@ -0,0 +1,54 @@
+
+# Dot-sourcing the Get-VsCodeExtensionMetadata function
+. powershell\functions\Get-VsCodeExtensionMetadata.ps1
+
+# Define the path to the extensions.json file
+$extensionsFilePath = Join-Path $HOME ".vscode/extensions/extensions.json"
+
+# Check if the file exists
+if (-Not (Test-Path $extensionsFilePath)) {
+    Write-Error -Exception "The file extensions.json does not exist at the specified path: $extensionsFilePath" -ErrorAction Stop
+}
+
+# Read the content of the extensions.json file
+$extensionsContent = Get-Content $extensionsFilePath -Raw
+
+# Parse the JSON content
+$extensionsJson = $extensionsContent | ConvertFrom-Json -Depth 5
+
+# Create a temp directory to save the .vsix files
+$vsixOutDirectory = "C:\temp\VsCodeExtensions\"
+if (-Not (Test-Path $vsixDirectory)) {
+    New-Item -ItemType Directory -Path $vsixOutDirectory 1> $null
+}
+
+$currentInstalledExtensions = Invoke-Expression "code --list-extensions --show-versions"
+
+$extensionsJson.ForEach{
+    $currentExtension = $_
+
+    $currentInstalledExtensions | Where-Object { $_.Split('@')[0] -eq $currentExtension.identifier.id }
+
+    $extensionInstalled = $currentInstalledExtensions | Where-Object { $_.StartsWith( $currentExtension.identifier.id) }
+    Write-Information "Extension [$($currentExtension.identifier.id)] is already installed"
+    $versionUpToDate = $extensionInstalled.Split('@')[1] -eq $currentExtension.version
+
+    if ($versionUpToDate) {
+        Write-Information "Extension version is also up to date, skipping installation"
+    } else {
+        $extensionMetadata = Get-VsCodeExtensionMetadata -ExtensionId $currentExtension.identifier.id
+        Write-Information "Imported extension [$($currentExtension.identifier.id)] market place metadata"
+
+        # compose a file name for the .vsix file
+        $composedFileName = "{0}_{1}_{2}.VSIX" -f $extensionMetadata.Publisher, $extensionMetadata.Name, $extensionMetadata.Version
+        $fullFilePath = "{0}{1}" -f $vsixOutDirectory, $composedFileName
+
+        # Download the .vsix file
+        Invoke-WebRequest -Uri $extensionMetadata.DownloadUri -OutFile $fullFilePath
+        Write-Information "Downloaded extension [$($extensionMetadata.Name)] to [$vsixOutDirectory]"
+
+        $destinationPath = Join-Path $vsixDirectory $fileName
+        Invoke-Expression "code --install-extension `"$destinationPath`""
+
+    }
+}
