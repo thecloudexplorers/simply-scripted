635e662f1b613f4b54806e77e80aea9d13920aa7
Jev <32514140+devjevnl@users.noreply.github.com>
2024-06-27 10:00:28 +0200

    First fully working version

--- powershell/installVsCodeExtensionsFromFile.ps1
+++ powershell/installVsCodeExtensionsFromFile.ps1
@@ -27,21 +27,19 @@ $currentInstalledExtensions = Invoke-Expression "code --list-extensions --show-v
 
 $extensionsJson.ForEach{
     $currentExtension = $_
-
     $currentInstalledExtensions | Where-Object { $_.Split('@')[0] -eq $currentExtension.identifier.id }
 
     $extensionInstalled = $currentInstalledExtensions | Where-Object { $_.StartsWith( $currentExtension.identifier.id) }
     if ($null -eq $extensionInstalled) {
         Write-Information "Extension [$($currentExtension.identifier.id)] is already installed"
-        $versionUpToDate = $extensionInstalled.Split('@')[1] -eq $currentExtension.version
-    }
-    if ($versionUpToDate) {
-        Write-Information "Extension version is also up to date, skipping installation"
-    } else {
+
+        # TODO: FIX VERSION COMPARISON
+        #$versionUpToDate = $extensionInstalled.Split('@')[1] -eq $currentExtension.version
+
         $extensionMetadata = Get-VsCodeExtensionMetadata -ExtensionId $currentExtension.identifier.id
         Write-Information "Imported extension [$($currentExtension.identifier.id)] market place metadata"
 
-        # compose a file name for the .vsix file
+        # Compose a file name for the .vsix file
         $composedFileName = "{0}_{1}_{2}.VSIX" -f $extensionMetadata.Publisher, $extensionMetadata.Name, $extensionMetadata.Version
         $fullFilePath = "{0}{1}" -f $vsixOutDirectory, $composedFileName
 
@@ -49,8 +47,7 @@ $extensionsJson.ForEach{
         Invoke-WebRequest -Uri $extensionMetadata.DownloadUri -OutFile $fullFilePath
         Write-Information "Downloaded extension [$($extensionMetadata.Name)] to [$vsixOutDirectory]"
 
-        $destinationPath = Join-Path $vsixDirectory $fileName
-        Invoke-Expression "code --install-extension `"$destinationPath`""
-
+        # install the extension
+        Invoke-Expression "code --install-extension `"$fullFilePath`""
     }
 }
