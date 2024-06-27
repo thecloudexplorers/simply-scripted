d4a70ff04a240a6ca8eea3d13a543f392219989b
Jev <32514140+devjevnl@users.noreply.github.com>
2024-06-27 11:07:17 +0200

    more fixes

--- powershell/installVsCodeExtensionsFromFile.ps1
+++ powershell/installVsCodeExtensionsFromFile.ps1
@@ -1,18 +1,48 @@
+#Requires -PSEdition Core
+<#
+    .SYNOPSIS
+    This script manages Visual Studio Code extensions by installing or updating them based on a provided JSON file.
+
+    .DESCRIPTION
+    The script reads a JSON file containing details about desired Visual Studio Code extensions.
+    It checks whether these extensions are already installed and up-to-date.
+    If an extension is not installed or is outdated, the script downloads the appropriate .vsix file and installs or updates the extension.
+    The script also creates a temporary directory to store the downloaded .vsix files if it does not already exist.
+
+    .PARAMETER ExtensionsJsonFilePath
+    The path to the JSON file containing the extensions' details. This parameter is mandatory.
+
+    .EXAMPLE
+    .\Manage-VsCodeExtensions.ps1 -ExtensionsJsonFilePath "C:\path\to\extensions.json"
+    This example reads the extensions.json file located at "C:\path\to\extensions.json" and manages the VS Code extensions accordingly.
+
+    .NOTES
+    Author      : Jev - @devjevnl | https://www.devjev.nl
+    Source      : https://github.com/thecloudexplorers/simply-scripted
+
+    Dependencies: Get-VsCodeExtensionMetadata.ps1 function file must be present in the specified path.
+#>
+
+# [CmdletBinding()]
+# param (
+#     [Parameter(Mandatory)]
+#     [System.String] $ExtensionsJsonFilePath
+# )
+
+$ExtensionsJsonFilePath = "C:\Temp\VsCodeExtensions\extensions.json"
+$InformationPreference = 'Continue'
 
 # Dot-sourcing the Get-VsCodeExtensionMetadata function
+Write-Information "Dot-sourcing the Get-VsCodeExtensionMetadata.ps1 function file"
 . powershell\functions\Get-VsCodeExtensionMetadata.ps1
 
-# Define the path to the extensions.json file
-#$extensionsFilePath = Join-Path $HOME ".vscode/extensions/extensions.json"
-$extensionsFilePath = "C:/Temp/VsCodeExtensions/extensions.json"
-
 # Check if the file exists
-if (-Not (Test-Path $extensionsFilePath)) {
-    Write-Error -Exception "The file extensions.json does not exist at the specified path: $extensionsFilePath" -ErrorAction Stop
+if (-Not (Test-Path $ExtensionsJsonFilePath)) {
+    Write-Error -Exception "The file extensions.json does not exist at the specified path: [$ExtensionsJsonFilePath]" -ErrorAction Stop
 }
 
 # Read the content of the extensions.json file
-$extensionsContent = Get-Content $extensionsFilePath -Raw
+$extensionsContent = Get-Content $ExtensionsJsonFilePath -Raw
 
 # Parse the JSON content
 $extensionsJson = $extensionsContent | ConvertFrom-Json -Depth 5
@@ -27,27 +57,37 @@ $currentInstalledExtensions = Invoke-Expression "code --list-extensions --show-v
 
 $extensionsJson.ForEach{
     $currentExtension = $_
-    $currentInstalledExtensions | Where-Object { $_.Split('@')[0] -eq $currentExtension.identifier.id }
 
-    $extensionInstalled = $currentInstalledExtensions | Where-Object { $_.StartsWith( $currentExtension.identifier.id) }
-    if ($null -eq $extensionInstalled) {
-        Write-Information "Extension [$($currentExtension.identifier.id)] is already installed"
+    Write-Information "Processing extension [$($currentExtension.identifier.id)]"
+    # Skip extensions that are not from the Visual Studio Code marketplace
+    if (-not $currentExtension.identifier.id.StartsWith("vscode.")) {
+        $currentInstalledExtensions | Where-Object { $_.Split('@')[0] -eq $currentExtension.identifier.id }
+        $extensionInstalled = $currentInstalledExtensions | Where-Object { $_.StartsWith( $currentExtension.identifier.id) }
+        if ($null -eq $extensionInstalled) {
+            Write-Information "Extension [$($currentExtension.identifier.id)] is not installed"
+            # TODO: EXtend this part to include functionality for updating out of date versions
+            #$versionUpToDate = $extensionInstalled.Split('@')[1] -eq $currentExtension.version
 
-        # TODO: FIX VERSION COMPARISON
-        #$versionUpToDate = $extensionInstalled.Split('@')[1] -eq $currentExtension.version
+            $extensionMetadata = Get-VsCodeExtensionMetadata -ExtensionId $currentExtension.identifier.id
+            Write-Information "Imported extension [$($currentExtension.identifier.id)] market place metadata"
 
-        $extensionMetadata = Get-VsCodeExtensionMetadata -ExtensionId $currentExtension.identifier.id
-        Write-Information "Imported extension [$($currentExtension.identifier.id)] market place metadata"
+            # Compose a file name for the .vsix file
+            $composedFileName = "{0}_{1}_{2}.VSIX" -f $extensionMetadata.Publisher, $extensionMetadata.Name, $extensionMetadata.Version
+            $fullFilePath = "{0}{1}" -f $vsixOutDirectory, $composedFileName
 
-        # Compose a file name for the .vsix file
-        $composedFileName = "{0}_{1}_{2}.VSIX" -f $extensionMetadata.Publisher, $extensionMetadata.Name, $extensionMetadata.Version
-        $fullFilePath = "{0}{1}" -f $vsixOutDirectory, $composedFileName
+            # Download the .vsix file
+            Invoke-WebRequest -Uri $extensionMetadata.DownloadUri -OutFile $fullFilePath
+            Write-Information "Downloaded extension [$($extensionMetadata.Name)] to [$vsixOutDirectory]"
 
-        # Download the .vsix file
-        Invoke-WebRequest -Uri $extensionMetadata.DownloadUri -OutFile $fullFilePath
-        Write-Information "Downloaded extension [$($extensionMetadata.Name)] to [$vsixOutDirectory]"
-
-        # install the extension
-        Invoke-Expression "code --install-extension `"$fullFilePath`""
+            # install the extension
+            $expressionOut = Invoke-Expression "code --install-extension `"$fullFilePath`""
+            $expressionOut.ForEach{
+                Write-Information $_
+            }
+        } else {
+            Write-Information "Extension [$($currentExtension.identifier.id)] is already installed"
+        }
+    } else {
+        Write-Information "Extension [$($currentExtension.identifier.id)] is not from the Visual Studio Code marketplace, skipping"
     }
 }
