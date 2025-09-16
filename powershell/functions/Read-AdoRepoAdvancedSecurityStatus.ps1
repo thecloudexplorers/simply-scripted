4b4911d0b283243edb33694a4e1ac4cce9f222c5
Jev Suchoi <jev.suchoi@TCSNLPS.onmicrosoft.com>
2025-09-16 11:56:40 +0200

    rewritten the logic to track status as a whole not just te enabled repos, added parser to resolve changed by id to a username

--- powershell/functions/Read-AdoRepoAdvancedSecurityStatus.ps1
+++ powershell/functions/Read-AdoRepoAdvancedSecurityStatus.ps1
@@ -12,7 +12,7 @@ function Read-AdoRepoAdvancedSecurityStatus {
     # Get the current status of Advanced Security for the organization
     # https://learn.microsoft.com/en-us/rest/api/azure/devops/advancedsecurity/org-enablement/get?view=azure-devops-rest-7.2&wt.mc_id=DT-MVP-5005327
     # GET https://advsec.dev.azure.com/{organization}/_apis/management/enablement?api-version=7.2-preview.3
-    $enablementUri = "https://advsec.dev.azure.com/$Organization/_apis/management/enablement?api-version=7.2-preview.3"
+    $enablementUri = "https://advsec.dev.azure.com/$Organization/_apis/management/enablement?includeAllProperties=true&api-version=7.2-preview.3"
 
     try {
 
@@ -30,8 +30,7 @@ function Read-AdoRepoAdvancedSecurityStatus {
             Write-Error "Access denied or token expired. Please verify your Bearer token is still valid." -ErrorAction Stop
 
         } else {
-            [System.Object[]]$secretProtectionEnabledRepos = $restResponse.reposEnablementStatus |
-            Where-Object { $_.secretProtectionFeatures.secretProtectionEnabled -eq $true }
+            [System.Collections.ArrayList]$secretProtectionEnabledRepos = $restResponse.reposEnablementStatus
 
             # Initialize result collection
             $repoAdvancedSecurityStatusCollection = [System.Collections.ArrayList]::new()
@@ -44,76 +43,66 @@ function Read-AdoRepoAdvancedSecurityStatus {
                 # Get project details
                 $projectUriTemplate = "https://dev.azure.com/{0}/_apis/projects/{1}?api-version=7.0"
                 $projectUri = $projectUriTemplate -f $Organization, $projectId
-                $projectInfo = Invoke-RestMethod -Uri $projectUri -Headers $AdoAuthenticationHeader -Method Get
+                $projectInfo = Invoke-RestMethod -Uri $projectUri -Headers $AdoAuthenticationHeader -Method 'GET'
 
                 # Get repository details
                 $repoUriTemplate = "https://dev.azure.com/{0}/{1}/_apis/git/repositories/{2}?api-version=7.0"
                 $repoUri = $repoUriTemplate -f $Organization, $projectId, $repoId
 
-                $repoInfo = Invoke-RestMethod -Uri $repoUri -Headers $AdoAuthenticationHeader -Method Get
+                $repoInfo = Invoke-RestMethod -Uri $repoUri -Headers $AdoAuthenticationHeader -Method 'GET'
+
+                # Get Secret Protection Changed By Display Name
+                $secretProtectionChangedByDisplayName = $null
+                if ($currentRepo.secretProtectionFeatures.secretProtectionChangedBy -ne "00000000-0000-0000-0000-000000000000") {
+                    $identityUriTemplate = "https://vssps.dev.azure.com/{0}/_apis/identities/{1}?api-version=7.2-preview.1"
+                    $identityUri = $identityUriTemplate -f $Organization, $currentRepo.secretProtectionFeatures.secretProtectionChangedBy
+
+                    $identityInfo = Invoke-RestMethod -Uri $identityUri -Headers $AdoAuthenticationHeader -Method 'GET'
+                    $secretProtectionChangedByDisplayName = $identityInfo.providerDisplayName
+                }
+
+                # Get Code Security Changed By Display Name
+                $codeSecurityChangedByDisplayName = $null
+                if ($currentRepo.codeSecurityFeatures.codeSecurityChangedBy -ne "00000000-0000-0000-0000-000000000000") {
+                    $identityUriTemplate = "https://vssps.dev.azure.com/{0}/_apis/identities/{1}?api-version=7.2-preview.1"
+                    $identityUri = $identityUriTemplate -f $Organization, $currentRepo.codeSecurityFeatures.codeSecurityChangedBy
+
+                    $identityInfo = Invoke-RestMethod -Uri $identityUri -Headers $AdoAuthenticationHeader -Method 'GET'
+                    $codeSecurityChangedByDisplayName = $identityInfo.providerDisplayName
+                }
 
                 # Add to result collection
                 [System.Void]$repoAdvancedSecurityStatusCollection.Add([PSCustomObject]@{
-                        ProjectId                 = $projectId
-                        ProjectName               = $projectInfo.name
-                        RepositoryId              = $repoId
-                        RepositoryName            = $repoInfo.name
-                        SecretProtectionEnabled   = $true
-                        DependencyScanningEnabled = $false
-                        CodeSecurityEnabled       = $false
+                        ProjectId                = $projectId
+                        ProjectName              = $projectInfo.name
+                        RepositoryId             = $repoId
+                        RepositoryName           = $repoInfo.name
+                        SecretProtectionFeatures = [PSCustomObject]@{
+                            SecretProtectionEnabled                   = $currentRepo.secretProtectionFeatures.secretProtectionEnabled
+                            SecretProtectionEnablementLastChangedDate = $currentRepo.secretProtectionFeatures.secretProtectionEnablementLastChangedDate
+                            SecretProtectionChangedBy                 = $secretProtectionChangedByDisplayName
+                            BlockPushes                               = $currentRepo.secretProtectionFeatures.blockPushes
+                        }
+                        CodeSecurityFeatures     = [PSCustomObject]@{
+                            CodeSecurityEnabled                = $currentRepo.codeSecurityFeatures.codeSecurityEnabled
+                            CodeSecurityLastChangedDate        = $currentRepo.codeSecurityFeatures.codeSecurityLastChangedDate
+                            CodeSecurityChangedBy              = $codeSecurityChangedByDisplayName
+                            DependencyScanningInjectionEnabled = $currentRepo.codeSecurityFeatures.dependencyScanningInjectionEnabled
+                        }
                     })
             }
 
-            [System.Object[]]$codeSecurityEnabledRepos = $restResponse.reposEnablementStatus |
-            Where-Object { $_.codeSecurityFeatures.codeSecurityEnabled -eq $true }
-
-            # Process Code Security enabled repositories
-            foreach ($currentRepo in $codeSecurityEnabledRepos) {
-                $projectId = $currentRepo.projectId
-                $repoId = $currentRepo.repositoryId
-
-                # Check if this repository is already in our results (has secretProtectionEnabledRepos enabled)
-                $existingRepo = $repoAdvancedSecurityStatusCollection | Where-Object { $_.RepositoryId -eq $repoId }
-
-                if ($existingRepo) {
-                    # Update existing entry
-                    $existingRepo.CodeSecurityEnabled = $currentRepo.codeSecurityFeatures.codeSecurityEnabled
-                    $existingRepo.DependencyScanningEnabled = $currentRepo.codeSecurityFeatures.dependencyScanningInjectionEnabled
-
-                } else {
-                    # Get project details
-                    $projectUriTemplate = "https://dev.azure.com/{0}/_apis/projects/{1}?api-version=7.0"
-                    $projectUri = $projectUriTemplate -f $Organization, $projectId
-                    $projectInfo = Invoke-RestMethod -Uri $projectUri -Headers $AdoAuthenticationHeader -Method Get
-
-                    # Get repository details
-                    $repoUriTemplate = "https://dev.azure.com/{0}/{1}/_apis/git/repositories/{2}?api-version=7.0"
-                    $repoUri = $repoUriTemplate -f $Organization, $projectId, $repoId
-                    $repoInfo = Invoke-RestMethod -Uri $repoUri -Headers $AdoAuthenticationHeader -Method Get
-
-                    # Add new entry
-                    [System.Void]$repoAdvancedSecurityStatusCollection.Add([PSCustomObject]@{
-                            ProjectId                 = $projectId
-                            ProjectName               = $projectInfo.name
-                            RepositoryId              = $repoId
-                            RepositoryName            = $repoInfo.name
-                            SecretProtectionEnabled   = $false
-                            CodeSecurityEnabled       = $currentRepo.codeSecurityFeatures.codeSecurityEnabled
-                            DependencyScanningEnabled = $currentRepo.codeSecurityFeatures.dependencyScanningInjectionEnabled
-                        })
-                }
-            }
-
             # Summary output
-            $reposWithSecretProtection = $repoAdvancedSecurityStatusCollection | Where-Object { $_.SecretProtectionEnabled -eq $true }
-            $reposWithCodeSecurity = $repoAdvancedSecurityStatusCollection | Where-Object { $_.CodeSecurityEnabled -eq $true }
-            Write-Information -Message "Total repositories with Secret Protection enabled: [$($reposWithSecretProtection.Count)]"
-            Write-Information -Message "Total repositories with Code Security enabled: [$($reposWithCodeSecurity.Count)]"
+            $reposWithOutSecretProtection = $repoAdvancedSecurityStatusCollection | Where-Object { $_.SecretProtectionFeatures.SecretProtectionEnabled -eq $false }
+            $reposWithOutCodeSecurity = $repoAdvancedSecurityStatusCollection | Where-Object { $_.CodeSecurityFeatures.CodeSecurityEnabled -eq $false }
+            Write-Information -Message "Repositories that have Secret Protection disabled:"
+            Write-Information -Message "[$($reposWithOutSecretProtection.Count) out of $($repoAdvancedSecurityStatusCollection.Count)]"
+
+            Write-Information -Message "Repositories that have Code Security disabled: "
+            Write-Information -Message "[$($reposWithOutCodeSecurity.Count) out of $($repoAdvancedSecurityStatusCollection.Count)]"
 
             # Return the result
             return  $repoAdvancedSecurityStatusCollection
-
-            Write-Host "Stopped here!!!"
         }
     } catch {
         Write-Error "Unexpected error occurred: $($_.Exception.Message)" -ErrorAction Stop
